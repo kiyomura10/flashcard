4ccdec79a6a2a8e994e376e99b9942e24ef8dc24
kiyomura10 <taiti199@yahoo.co.jp>
2023-06-23 22:25:33 +0900

    ロゴの設定

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,9 +4,9 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="shrink-0 flex items-center ">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <p class="block h-9 w-auto fill-current text-gray-800 text-2xl font-mduiem" ><span class="text-red-500">Q</span>uize Maker</p>
                     </a>
                 </div>
 
